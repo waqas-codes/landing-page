e5d21a8632cf0898b90fc5d74be52186720f32f3
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-10-29 23:39:41 +0500

    Home componenet is ready to push

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import '../App.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='hero-section'>
+      <div className="content">
+        <h1>WE ARE A LANDING PAGE</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sed commodo nibh ante facilisis bibendum.</p>
+      <button>LEARN MORE</button>
+      </div>
+    </div>
   )
 }
 
