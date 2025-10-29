97734bb05fae152e4429e94860d868bd8864591c
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-10-29 23:42:40 +0500

    Feature component is on working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Feature from './components/Feature'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home />
+      <Feature />
     </div>
   )
 }
