5925f0d5c1c2983e9bc2c59760b5d077a3819fea
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-11-05 20:51:12 +0500

    gallery component in on working

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import GalleryComponent from './icons-container/GalleryComponent'
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <div>
+      <div className="text-container">
+        <h1>GALLERY</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
+      </div>
+      <GalleryComponent />
+    </div>
   )
 }
 
--- src/components/icons-container/GalleryComponent.jsx
+++ src/components/icons-container/GalleryComponent.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const GalleryComponent = () => {
   return (
-    <div>GalleryComponent</div>
+    <div>
+      
+    </div>
   )
 }
 
