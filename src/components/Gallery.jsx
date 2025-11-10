c2028edaae8c09fa57a56195a5bbcbc87930f55e
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-11-10 15:30:00 +0500

    gallery component is on working

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -11,12 +11,6 @@ const Gallery = () => {
       <GalleryComponent 
       bgImage="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
       />
-      <GalleryComponent 
-      bgImage="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
-      />
-      <GalleryComponent 
-      bgImage="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
-      />
     </div>
   )
 }
