bfe589726792927869438e2b8f0c7273a1c0b8e6
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-11-12 00:11:57 +0500

    gallery component is on working

--- src/components/icons-container/GalleryComponent.jsx
+++ src/components/icons-container/GalleryComponent.jsx
@@ -1,23 +1,11 @@
 import React from 'react'
+import styles from './Gallery.module.css';
 
 const GalleryComponent = ({bgImage, title}) => {
 
-    const sectionStyle = {
-    backgroundImage: `url(${bgImage})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    width: "374px",
-    height: "136px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    color: "#fff", // text color on image
-    textShadow: "0 2px 5px rgba(0,0,0,0.6)"
-  }
   return (
     <div>
-      <div className="image" style={sectionStyle}>
+      <div className="image" style={styles}>
 
       </div>
     </div>
